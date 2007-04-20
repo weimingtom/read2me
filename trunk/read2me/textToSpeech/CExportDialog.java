b7c96a51e45724c0e48db4d348bb9b9d08bfb212
mickael.meyer <mickael.meyer@424d2462-4a28-0410-b1ca-e5c1fdeeaee5>
2007-04-20 21:12:55 +0000

    audio file exported where the user wants (but wav file still not released)

--- trunk/read2me/textToSpeech/CExportDialog.java
+++ trunk/read2me/textToSpeech/CExportDialog.java
@@ -57,31 +57,33 @@ public class CExportDialog {
 		if( fileName.endsWith("mp3") ){
 
 			System.out.println("InstallationDirectory: " +System.getProperty("user.dir"));
-			String encodePath = System.getProperty("user.dir")+ "\\temp.wav";
+			//String encodePath = System.getProperty("user.dir")+ "\\temp.wav";
+			
 
 			fileName = fileName.replace(".mp3", ".wav");
 			path = path +"\\"+ fileName;
 			System.out.println("filename: "+path);
 			//CExportToWAV createWAV = new CExportToWAV("change default text to mp3 textToConvert",path);
 
+			/*
 			//saves the audio files in the install directory. Useful for future move of file
-			//CExportToWAV createWAV = new CExportToWAV(textToConvert,encodePath);
-			//Mp3Encoder encoder = new Mp3Encoder(encodePath,"16");
-			//CExportToWAV createWAV = new CExportToWAV(textToConvert,path);
-
-
 			CFullExportToWAV createWAV = new CFullExportToWAV(textToConvert,encodePath, voice);
 			System.out.println("Wav path: "+encodePath);
-
-
 			Mp3Encoder encoder = new Mp3Encoder(encodePath,"16");
+			*/
+			
 
+			CFullExportToWAV createWAV = new CFullExportToWAV(textToConvert,path, voice);
+			System.out.println("Wav path: "+path);
+
+			Mp3Encoder encoder = new Mp3Encoder(path,"16");
+			
 			createWAV.exportToWAV();
 
 			//encoder.start();
 			encoder.beginMP3Export();
 
-
+/*
 			//moves the generated mp3 file to the location chosen by the user
 			encodePath = encodePath.replace(".wav", ".mp3");
 			path = path.replace(".wav", ".mp3");
@@ -100,7 +102,7 @@ public class CExportDialog {
 				// File was not successfully renamed
 				System.out.println("move failed");
 			}
-
+*/
 
 
 			/*File myFile = new File(encodePath);
